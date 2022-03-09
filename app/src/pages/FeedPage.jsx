e41678f17bc1aaae54028f117b07a55024e64e72
Anna Rylander <annarylander@AirsomtlhorAnna.lan>
2022-03-09 21:37:29 +0100

    styling feed

--- app/src/pages/FeedPage.jsx
+++ app/src/pages/FeedPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Feed from '../components/Feed'
-import Logout from '../components/Logout'
 import PostCreate from '../components/PostCreate'
-import Profile from '../components/Profile'
 
 export default function () {
   return (
